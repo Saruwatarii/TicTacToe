031e62bc12e8830ffad0865ac1d9fd33d91d287d
Tim Aadland <potetstapp@gmail.com>
2020-07-16 15:15:54 +0200

    Added decription to the game and printed out a random game display

---
+++ tic_tac_toe.py
@@ -0,0 +1,10 @@
+'''
+Tic-tac-toe is a game played by two players on a 3x3 field where the duel takes place. One of the players plays as 'X',
+and the other player is 'O'. 'X' plays first, then the 'O' side plays, and so on.
+The first player that writes 3 'X' or 3 'O' in a straight line (including diagonals) wins.
+'''
+# Random state of the board
+print('''X O X
+O X O
+X X O
+''')
