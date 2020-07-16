0cc314d49acb4e28c79796c5cd0ad4393618ef15
Tim Aadland <potetstapp@gmail.com>
2020-07-16 15:55:01 +0200

    Added  input from the user to determine the game and boundaries

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -3,8 +3,14 @@ Tic-tac-toe is a game played by two players on a 3x3 field where the duel takes 
 and the other player is 'O'. 'X' plays first, then the 'O' side plays, and so on.
 The first player that writes 3 'X' or 3 'O' in a straight line (including diagonals) wins.
 '''
-# Random state of the board
-print('''X O X
-O X O
-X X O
-''')
+
+
+# Visualize different combinations that the user will determine from the input
+
+interface = input("Enter cells: ")
+
+print("---------")
+print("| " + (interface[0]) + " " + (interface[1]) + " " + (interface[2]) + " |\n"
+      "| " + (interface[3]) + " " + (interface[4]) + " " + (interface[5]) + " |\n"
+      "| " + (interface[6]) + " " + (interface[7]) + " " + (interface[-1]) + " |")
+print("---------")
