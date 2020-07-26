15972d3f7e4ad2f93a2c25841017379eace53be1
Tim Aadland <potetstapp@gmail.com>
2020-07-26 17:39:52 +0200

    Added the ability to select a cell for the user move. Also added error message regarding situation like: if the cell is not empty, user enter other symbols, or ifthe user goes beyond the field.

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -19,6 +19,120 @@ print("|", cells[3], cells[4], cells[5], "|")
 print("|", cells[6], cells[7], cells[-1], "|")
 print("---------")
 
+user_move = input("Enter the coordinates: ")
+
+while True:
+
+    if user_move[0].isdigit() and user_move[2].isdigit():
+        break
+    else:
+        print("You should enter numbers!")
+        user_move = input("Enter the coordinates: ")
+        True
+
+higher = False
+
+while True:
+    if int(user_move[0]) > 3 or int(user_move[2]) > 3:
+        print("Coordinates should be from 1 to 3!")
+        user_move = input("Enter the coordinates: ")
+        True
+    else:
+        break
+
+flag = True
+while flag:
+
+    if user_move == "1 3":
+        if cells[0] == "X" or cells[0] == "O":
+            print("This cell is occupied! Choose another one!")
+            flag
+            user_move = input("Enter the coordinates: ")
+        elif cells[0] != "X" or cells[0] != "O":
+            cells[0] = "X"
+            flag = False
+
+    if user_move == "1 2":
+        if cells[3] == "X" or cells[3] == "O":
+            print("This cell is occupied! Choose another one!")
+            user_move = input("Enter the coordinates: ")
+            flag
+        elif cells[3] != "X" or cells[3] != "O":
+            cells[3] = "X"
+            flag = False
+
+    if user_move == "1 1":
+        if cells[6] == "X" or cells[6] == "O":
+            print("This cell is occupied! Choose another one!")
+            user_move = input("Enter the coordinates: ")
+            flag
+        elif cells[6] != "X" or cells[6] != "O":
+            cells[6] = "X"
+            flag = False
+
+    if user_move == "2 1":
+        if cells[7] == "X" or cells[7] == "O":
+            print("This cell is occupied! Choose another one!")
+            user_move = input("Enter the coordinates: ")
+            flag
+        elif cells[7] != "X" or cells[7] != "O":
+            cells[7] = "X"
+            flag = False
+    if user_move == "2 2":
+        if cells[4] == "X" or cells[4] == "O":
+            print("This cell is occupied! Choose another one!")
+            user_move = input("Enter the coordinates: ")
+            flag
+        elif cells[4] != "X" or cells[4] != "O":
+            cells[4] = "X"
+            flag = False
+
+    if user_move == "2 3":
+        if cells[1] == "X" or cells[1] == "O":
+            print("This cell is occupied! Choose another one!")
+            user_move = input("Enter the coordinates: ")
+            flag
+        elif cells[1] != "X" or cells[1] != "O":
+            cells[1] = "X"
+            flag = False
+
+    if user_move == "3 1":
+        if cells[-1] == "X" or cells[-1] == "O":
+            print("This cell is occupied! Choose another one!")
+            user_move = input("Enter the coordinates: ")
+            flag
+        elif cells[-1] != "X" or cells[-1] != "O":
+            cells[-1] = "X"
+            flag = False
+
+    if user_move == "3 2":
+        if cells[5] == "X" or cells[5] == "O":
+            print("This cell is occupied! Choose another one!")
+            user_move = input("Enter the coordinates: ")
+            flag
+        elif cells[5] != "X" or cells[5] != "O":
+            cells[5] = "X"
+            flag = False
+
+    if user_move == "3 3":
+        if cells[2] == "X" or cells[2] == "O":
+            print("This cell is occupied! Choose another one!")
+            user_move = input("Enter the coordinates: ")
+            flag
+        elif cells[2] != "X" or cells[2] != "O":
+            cells[2] = "X"
+            flag = False
+
+
+print("---------")
+print("|", cells[0], cells[1], cells[2], "|")
+print("|", cells[3], cells[4], cells[5], "|")
+print("|", cells[6], cells[7], cells[-1], "|")
+print("---------")
+
+
+
+'''
 row0 = [cells[0], cells[1], cells[2]]
 row1 = [cells[3], cells[4], cells[5]]
 row2 = [cells[6], cells[7], cells[-1]]
@@ -67,3 +181,4 @@ elif (win_count_x == 1 and win_count_o == 1) or ((x_count - o_count) >= 2 or (o_
 elif win_count_x == 0 and win_count_o == 0 and blanks_count > 0:
     print('Game not finished')
 
+'''
